1ec6d87293dc68a1a1bebfc2b1f184bb9b1cd7bc
Alessandro Dutra <cachopaweb@gmail.com>
2023-02-09 01:03:52 -0400

    update readme

--- README.md
+++ README.md
@@ -1,22 +1,18 @@
-<img src="https://storage.googleapis.com/golden-wind/experts-club/capa-github.svg" />
+<img src="https://drive.google.com/uc?id=1XPWLjUo2-j8iGw07ALcxu7oqJ3nkl2Ho" alt="Rocketseat+"/>
 
-# Título
+# Animando cards no Flutter com Transform
 
-Introdução ao seu projeto. Apresentado por [Seu nome][1].
+Neste projeto é apresentado como usar o widget [Transform][3] no [Flutter][2], para animar objetos em 3D. Nesta aula foi criado um projeto simulando um cartão de Crédito no qual o usuário pode animar o cartão no evento de Drag (Click-arrasta). 
 
-> __Atenção__: Este é o projeto <template/completo>, se você está procurando o projeto de <completo/template>, vá para [este repositório](https://github.com/rocketseat-experts-club/<seu projeto aqui>)
-
-## Instalação
-
-1. Use este template
-2. Edite o README.md
-3. Adicione seus arquivos
+Apresentado por [Alessandro Dutra de Andrade (Cachopaweb)][1].
 
 ## Expert
 
-| [<img src="https://github.com/<seu handle>.png" width="75px;"/>][1] |
+| [<img src="https://github.com/cachopaweb.png" width="75px;"/>][1] |
 | :-: |
 |[Creator][1]|
 
 
-[1]: https://seusite.com.br
+[1]: https://github.com/cachopaweb
+[2]: https://flutter.dev/
+[3]: https://api.flutter.dev/flutter/widgets/Transform-class.html
---
+++ lib/widgets/credit_card_widget.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class CreditCardWidget extends StatefulWidget {
+  const CreditCardWidget({super.key});
+
+  @override
+  State<CreditCardWidget> createState() => _CreditCardWidgetState();
+}
+
+class _CreditCardWidgetState extends State<CreditCardWidget> {
+  bool isFront = true;
+  double angle = 0;
+
+  Widget _buildCardFront() {
+    return Transform(
+      alignment: Alignment.center,
+      transform: Matrix4.identity()
+        ..setEntry(3, 2, 0.001)
+        ..rotateX(angle / 180 * pi),
+      child: Image.asset('images/card-front.png'),
+    );
+  }
+
+  Widget _buildCardBack() {
+    return Transform(
+      alignment: Alignment.center,
+      transform: Matrix4.identity()
+        ..setEntry(3, 2, 0.001)
+        ..rotateX(
+          angle / 180 * pi,
+        ),
+      child: Image.asset('images/card-back.png'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: 300,
+        height: 300,
+        child: GestureDetector(
+          onVerticalDragUpdate: (details) {
+            setState(() {
+              angle += details.delta.dy;
+              angle %= 360;
+              isFront = (angle <= 90 || angle >= 270);
+            });
+          },
+          child: isFront ? _buildCardFront() : _buildCardBack(),
+        ),
+      ),
+    );
+  }
+}
